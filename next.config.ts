05357be2b1f6736f49226c7deb922753cba64014
aMarketology <ironoaktexas@gmail.com>
2026-06-04 18:49:52 -0500

    redirect: 301 /tech-meta-instagram-drops-encrypted-dms -> ozonenetwork.news

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "**" },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/tech-meta-instagram-drops-encrypted-dms',
+        destination: 'https://www.ozonenetwork.news/social/meta/instagram-drops-encrypted-dms',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
